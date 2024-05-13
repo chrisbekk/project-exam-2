f0c5ad9f510c75528530d7c6233a19bc47f397f7
chrisbekk <christianbekkelund@gmail.com>
2024-05-13 20:55:54 +0200

    Refactoring filter venues functionality

--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -3,8 +3,9 @@ import { FilterVenues } from '../components/FilterVenues';
 import { useFetchVenues } from '../context/useFetchVenues';
 import { Button } from '../components/Button';
 import { VenuesContainer } from '../components/VenuesContainer';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
+import FilterResults from '../components/FilterResults';
 export const Venues = () => {
   const { venues, error, pending } = useFetchVenues();
 
@@ -13,39 +14,41 @@ export const Venues = () => {
   const [destination, setDestination] = useState(
     queryParams.get('destination'),
   );
-  const [fromDate, setFromDate] = useState(queryParams.get('fromDate'));
-  const [toDate, setToDate] = useState(queryParams.get('toDate'));
-  const [guests, setGuests] = useState(queryParams.get('guests'));
+  const [rating, setRating] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [guests, setGuests] = useState(null);
 
-  let filteredVenues = venues?.data;
+  const filteredVenues = useMemo(() => {
+    let filteredVenues = venues?.data;
 
-  // Filter venues by destination
-  if (destination !== '') {
-    filteredVenues = filteredVenues?.filter(
-      venue => venue.location.city?.toLowerCase() === destination.toLowerCase(),
-    );
-  }
-
-  // Filter venues by availability between specified dates
-  if (fromDate && toDate) {
-    filteredVenues = filteredVenues?.filter(venue => {
-      // Check if venue has any bookings that overlap with the specified date range
-      const hasOverlappingBookings = venue.bookings.some(booking => {
-        return (
-          (booking.dateFrom <= fromDate && booking.dateTo >= fromDate) ||
-          (booking.dateFrom <= toDate && booking.dateTo >= toDate) ||
-          (booking.dateFrom >= fromDate && booking.dateTo <= toDate)
-        );
+    // Filter venues by destination
+    if (destination !== '') {
+      filteredVenues = filteredVenues?.filter(
+        venue =>
+          venue.location.city?.toLowerCase() === destination.toLowerCase(),
+      );
+    }
+    //Sort filtered venues by price
+    if (price) {
+      filteredVenues = filteredVenues?.sort((low, high) => {
+        return low.price - high.price;
       });
+    }
 
-      return !hasOverlappingBookings; // Exclude venues with overlapping bookings
-    });
-  }
-
+    return filteredVenues;
+  }, [venues, destination, price]);
+  console.log(filteredVenues);
   return (
     <>
-      <div className="px-2"></div>
-
+      <div className="px-2">{destination}</div>
+      <Section>
+        {destination !== '' && (
+          <FilterResults
+            filteredVenues={filteredVenues}
+            destination={destination}
+          />
+        )}
+      </Section>
       <Section>
         <VenuesContainer
           venues={filteredVenues}
