2b80a8e0af3b0d05b92b7019c7cadda15159828e
chrisbekk <christianbekkelund@gmail.com>
2024-05-15 17:54:05 +0200

    Working on pagination and reworking filter function on venues page

--- src/components/FilterVenues/index.jsx
+++ src/components/FilterVenues/index.jsx
@@ -2,8 +2,21 @@ import { useRef } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
 import { GrMoney } from 'react-icons/gr';
 import { HiStar } from 'react-icons/hi';
-export default function FilterVenues({ filter, setFilter, setDestination }) {
+export default function FilterVenues({
+  filter,
+  setFilter,
+  metaFilter,
+  setMetaFilter,
+  setDestination,
+}) {
   const destinationRef = useRef(null);
+  let metaFiltersArray = [];
+  for (let key in metaFilter) {
+    if (metaFilter[key]) {
+      metaFiltersArray.push(key);
+    }
+  }
+
   const handleClick = e => {
     if (filter === e.target.id) {
       setFilter(null);
@@ -16,11 +29,28 @@ export default function FilterVenues({ filter, setFilter, setDestination }) {
     setDestination(destinationRef.current.value);
     destinationRef.current.value = '';
   };
+  const handleMetaFilter = e => {
+    for (let key in metaFilter) {
+      if (e.target.id === key) {
+        setMetaFilter(prevMetaFilter => ({
+          ...prevMetaFilter,
+          [e.target.id]: !prevMetaFilter[e.target.id],
+        }));
+      }
+    }
+  };
   const handleReset = () => {
     setFilter(null);
     setDestination('');
+    setMetaFilter({
+      wifi: false,
+      parking: false,
+      breakfast: false,
+      pets: false,
+    });
     destinationRef.current.value = '';
   };
+  console.log(metaFiltersArray);
   return (
     <div className=" max-w-[764px] rounded-md sm:rounded-3xl sm:bg-white sm:p-10 sm:shadow-lg md:mx-auto">
       <form onSubmit={handleSubmit}>
@@ -62,6 +92,36 @@ export default function FilterVenues({ filter, setFilter, setDestination }) {
           className="size-10 rounded-full bg-brand text-sm text-white transition-colors hover:bg-rose-600"
         >
           X
+        </button>
+      </div>
+      <div className="mt-4 flex gap-4">
+        <button
+          id="wifi"
+          onClick={handleMetaFilter}
+          className={`${metaFilter.wifi ? 'bg-brand text-white shadow-inner hover:bg-red-600' : 'bg-neutral-300 text-black hover:bg-neutral-400'} sm:text-md rounded-xl p-2 text-sm transition-colors`}
+        >
+          Wifi
+        </button>
+        <button
+          id="parking"
+          onClick={handleMetaFilter}
+          className={`${metaFilter.parking ? 'bg-brand text-white shadow-inner hover:bg-red-600' : 'bg-neutral-300 text-black hover:bg-neutral-400'} sm:text-md rounded-xl p-2 text-sm transition-colors`}
+        >
+          Parking
+        </button>
+        <button
+          id="breakfast"
+          onClick={handleMetaFilter}
+          className={`${metaFilter.breakfast ? 'bg-brand text-white shadow-inner hover:bg-red-600' : 'bg-neutral-300 text-black hover:bg-neutral-400'} sm:text-md rounded-xl p-2 text-sm transition-colors`}
+        >
+          Breakfast
+        </button>
+        <button
+          id="pets"
+          onClick={handleMetaFilter}
+          className={`${metaFilter.pets ? 'bg-brand text-white shadow-inner hover:bg-red-600' : 'bg-neutral-300 text-black hover:bg-neutral-400'} sm:text-md rounded-xl p-2 text-sm transition-colors`}
+        >
+          Pets
         </button>
       </div>
       {filter && (
--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -3,12 +3,15 @@ import FilterVenues from '../components/FilterVenues';
 import { useFetchVenues } from '../context/useFetchVenues';
 import { Button } from '../components/Button';
 import { VenuesContainer } from '../components/VenuesContainer';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import FilterResults from '../components/FilterResults';
 import filterVenuesByDestination from '../utils/filterVenues';
+import Pagination from '../components/Pagination';
+
 export const Venues = () => {
-  const { venues, error, pending } = useFetchVenues();
+  const [fetchPage, setFetchPage] = useState(1);
+  const { venues, error, pending } = useFetchVenues(50, fetchPage);
   console.log(venues);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -16,10 +19,22 @@ export const Venues = () => {
     queryParams.get('destination'),
   );
   const [filter, setFilter] = useState(null);
+  const [metaFilter, setMetaFilter] = useState({
+    wifi: null,
+    parking: null,
+    breakfast: null,
+    pets: null,
+  });
+  const [applyMetaFilter, setApplyMetaFilter] = useState(false); // Flag to apply metaFilter
 
   let filteredVenues = useMemo(() => {
     return filterVenuesByDestination(venues?.data, destination);
-  }, [venues, destination]);
+  }, [venues, destination, fetchPage]);
+
+  // Effect to apply metaFilter when it changes
+  useEffect(() => {
+    setApplyMetaFilter(true); // Set flag to apply metaFilter
+  }, [metaFilter]);
 
   if (filter) {
     if (filter === 'price') {
@@ -33,12 +48,29 @@ export const Venues = () => {
     }
   }
 
+  // Filter Venues by user defined tags
+  filteredVenues = filteredVenues?.filter(venue => {
+    if (!applyMetaFilter) return true; // If flag is false, include all venues
+    for (const key in metaFilter) {
+      if (metaFilter[key] !== null && metaFilter[key] !== venue.meta[key]) {
+        return false; // If metaFilter is not null and venue's meta value doesn't match, exclude venue
+      }
+    }
+    return true; // Include venue if all metaFilter values are null or match venue's meta values
+  });
+
+  const fetchNextPage = () => {
+    setFetchPage(prev => prev + 1);
+  };
+
   return (
     <>
       <div className="px-2">
         <FilterVenues
           filter={filter}
           setFilter={setFilter}
+          metaFilter={metaFilter}
+          setMetaFilter={setMetaFilter}
           setDestination={setDestination}
         />
       </div>
@@ -57,7 +89,7 @@ export const Venues = () => {
           pending={pending}
         />
       </Section>
-      <Button>View More</Button>
+      <Pagination meta={venues?.meta} setFetchPage={setFetchPage} />
     </>
   );
 };
