d91ed1fc87799b58e5dece53e761c4b8c7f7fafb
chrisbekk <christianbekkelund@gmail.com>
2024-05-18 10:25:13 +0200

    Fixing image sizes and height overflowing on venue details page

--- src/components/VenueDetails/Media/Gallery.jsx
+++ src/components/VenueDetails/Media/Gallery.jsx
@@ -1,37 +1,38 @@
 import React, { useState } from 'react';
-import { IoCloseCircleOutline } from 'react-icons/io5';
 import { createPortal } from 'react-dom';
+import { Button } from '../../Button';
 export const Gallery = ({ isVisible, setIsVisible, gallery }) => {
   const [currentImage, setCurrentImage] = useState(0);
   if (!isVisible) return;
-
+  console.log(isVisible);
   return createPortal(
-    <div className="absolute inset-0 flex min-h-screen items-center justify-center bg-neutral-50 bg-opacity-90">
-      <div className="max-w-[540px] pt-20">
-        <div className="mb-4 flex w-full items-center justify-end">
-          <button onClick={() => setIsVisible(false)}>
-            <IoCloseCircleOutline />
-          </button>
-        </div>
-        <div className="h-[50vh] ">
+    <div className="absolute inset-0 flex items-center justify-center bg-neutral-50">
+      <div className="w-full pt-24">
+        <div className=" w-full  sm:px-6 xl:mx-auto xl:w-[1280px]">
           <img
             src={gallery[currentImage]?.url}
-            className="h-full object-cover"
+            className="h-full w-full object-contain sm:rounded-md"
           />
         </div>
-        <div className=" grid grid-cols-3 gap-1 pt-10">
+        <div className=" mx-auto grid max-w-[420px] grid-cols-6  gap-1 border border-red-500 pt-10">
           {gallery.map((image, index) => {
             return (
               <div
                 key={index}
                 role="button"
                 onClick={() => setCurrentImage(index)}
-                className={`${index === currentImage ? 'opacity-100' : 'opacity-60'} rounded-md`}
+                className={`${index === currentImage ? 'opacity-100' : 'opacity-60'} min-h-full rounded-md`}
               >
-                <img src={image?.url} className="rounded-md" />
+                <img
+                  src={image?.url}
+                  className="aspect-square rounded-md object-cover"
+                />
               </div>
             );
           })}
+        </div>
+        <div className="mt-10 w-full px-4 xs:mx-auto xs:max-w-[340px] xs:px-0">
+          <Button handleClick={() => setIsVisible(false)}>Close</Button>
         </div>
       </div>
     </div>,
