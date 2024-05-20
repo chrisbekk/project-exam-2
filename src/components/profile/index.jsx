20ffb276f4ebf9e071c8316f263568c938753059
chrisbekk <christianbekkelund@gmail.com>
2024-05-20 10:07:46 +0200

    Profile Page init

---
+++ src/components/profile/index.jsx
@@ -0,0 +1,3 @@
+export default function ProfilePage({ data }) {
+  return <div>Profile Page</div>;
+}
--- src/pages/auth/Profile.jsx
+++ src/pages/auth/Profile.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
-
+import { Section } from '../../components/Section';
+import { useAuthContext } from '../../context/authContext';
 import React from 'react';
 
 export const Profile = () => {
+  const { data } = useAuthContext();
+  console.log(data);
   return (
     <>
-      <h1>Profile Page</h1>
-      <Link to={'/'}>Home</Link>
+      <Section ySpace={false}>
+        <h1>Profile Page</h1>
+      </Section>
     </>
   );
 };
